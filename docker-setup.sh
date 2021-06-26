b4109a20244c8b7df7573c17dc93115e62e83e36
Christian <cesium2349@protonmail.com>
2021-06-26 13:25:15 -0400

    protect the user from the user

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# make sure *.php are in /var/www/html
+if ! [ -f /.dockerenv ]; then
+    echo "This script is made to be run automatically inside the container."
+    exit
+fi
 cd /var/www
 mv -f *.php ./html/
 chmod a+rw /var/www/html
