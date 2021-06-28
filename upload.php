ed6cfacd1b554f7877655114d5ae5a5c760dc494
Christian <cesium2349@protonmail.com>
2021-06-28 13:17:22 -0400

    theres useful stuff in this commit maybe idk i forget whats going on

--- upload.php
+++ upload.php
@@ -3,24 +3,24 @@
 # "config" variables
 $URL = "https://cesium.one/i/"; # should have trailing slash
 
-    if(isset($_FILES['image'])){
-        $errors= array();
-        $file_size = $_FILES['image']['size'];
-        $file_name = $_FILES['image']['name'];
-        $file_tmp = $_FILES['image']['tmp_name'];
-        $file_type = $_FILES['image']['type'];
-        $file_ext = strtolower(end(explode('.',$_FILES['image']['name'])));
+if ((isset($_FILES['image'])) && ($_FILES['image']['size'] > 0))  {
+	$errors= array();
+	$file_size = $_FILES['image']['size'];
+	$file_name = $_FILES['image']['name'];
+	$file_tmp = $_FILES['image']['tmp_name'];
+	$file_type = $_FILES['image']['type'];
+	$file_ext = strtolower(end(explode('.',$_FILES['image']['name'])));
 
-        if(empty($errors)==true) {
-            $new_name = md5_file($file_tmp).".".$file_ext;
-            if($file_ext==$file_name) {
-                $new_name = md5_file($file_tmp);
-            }
-            move_uploaded_file($file_tmp,"./".$new_name);
-        } else {
-            print_r($errors);
-        }
-    }
+	if(empty($errors)==true) {
+		$new_name = md5_file($file_tmp).".".$file_ext;
+		if($file_ext==$file_name) {
+			$new_name = md5_file($file_tmp);
+		}
+		move_uploaded_file($file_tmp,"./".$new_name);
+	} else {
+	print_r($errors);
+	}
+}
 
 # display only the link to the upload if we're receiving it from cURL
 $UA=$_SERVER['HTTP_USER_AGENT'];
